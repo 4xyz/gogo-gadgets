6412484739f6e7b58ca90e4a846701065d9f82db
Tao Li <tli@exablox.com>
2021-07-12 08:45:23 -0700

    ML-iris workspace setup

--- machine-learning/iris/python3/setup-env.sh
+++ machine-learning/iris/python3/setup-env.sh
@@ -4,8 +4,8 @@ set -ex
 
 MY_DIR=$(dirname "$0")
 
-python3 -m venv $MY_DIR/.env
-source $MY_DIR/.env/bin/activate
+python3 -m venv $MY_DIR/iris-venv
+source $MY_DIR/iris-venv/bin/activate
 pip install --upgrade pip
 pip install wheel
 pip install -r $MY_DIR/requirements.txt
